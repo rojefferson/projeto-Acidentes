52baf2724270ca655bcf72c4eb0d750c4e1d0f27
rojefferson <jrm@cin.ufpe.br>
2018-06-10 06:26:20 -0300

    arquivo com  algumas ajudas na hora de programar

---
+++ comandosAux.py
@@ -0,0 +1,17 @@
+
+# plt.plot([0,10,20,30])
+# plt.show()
+
+#select 
+
+#data.loc[(data["Gender"]=="Female") & (data["Education"]=="Not Graduate") & (data["Loan_Status"]=="Y"), ["Gender","Education","Loan_Status"]]
+
+#visão inicial dos dados
+#pd.crosstab(data["Credit_History"],data["Loan_Status"],margins=True)
+
+#tamanho da base
+#acidentes.shape
+
+
+#conta os dados 
+#print acidentes['situacao'].value_counts()
