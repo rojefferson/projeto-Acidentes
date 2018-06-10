3ffd95fee7f8822e9c7b5770aa587688ced1a908
rojefferson <jrm@cin.ufpe.br>
2018-06-10 05:47:55 -0300

    versão plotando grafico teste

--- projeto.py
+++ projeto.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-
+plt.plot([0,10,20,30])
+plt.show()
 acidentes = pd.read_csv(
     "acidentes2017.csv",
     sep=';',header=None, names=["tipo","situacao","data","hora","bairro","endereco","numero","complemento","natureza","descricao","auto","moto","ciclom","ciclista","pedestre","onibus","caminhao","viatura","outros","vitimas","vitimasfatais"])
