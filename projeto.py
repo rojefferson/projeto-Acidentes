3c4da9742b346f5917ec3544b4ea09fd2ad2ec4f
rojefferson <jrm@cin.ufpe.br>
2018-06-10 05:25:34 -0300

    primeira versão rodando a base

--- projeto.py
+++ projeto.py
@@ -2,6 +2,10 @@ import numpy as np
 import pandas as pd
 
 
+acidentes = pd.read_csv(
+    "acidentes2017.csv",
+    sep=';',header=None, names=["tipo","situacao","data","hora","bairro","endereco","numero","complemento","natureza","descricao","auto","moto","ciclom","ciclista","pedestre","onibus","caminhao","viatura","outros","vitimas","vitimasfatais"])
+
 
 
 
