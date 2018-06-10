b58e43cac4304ad1b96640e3469bd41b3461c614
rojefferson <jrm@cin.ufpe.br>
2018-06-10 08:43:18 -0300

    alterações

--- projeto.py
+++ projeto.py
@@ -1,13 +1,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import xlsxwriter 
+
 plt.style.use("ggplot")
 
 acidentes = pd.read_csv(
     "acidentes2017.csv",
     sep=';',header=None, names=["tipo","situacao","data","hora","bairro","endereco","numero","complemento","natureza","descricao","auto","moto","ciclom","ciclista","pedestre","onibus","caminhao","viatura","outros","vitimas","vitimasfatais"])
 
-
-tipoCarro = pd.crosstab([acidentes['tipo'],acidentes['situacao']], acidentes['vitimas'],margins=True)
-
-print tipoCarro
