c86b02c6e83660f1c5ad013b93f78a72da580853
rojefferson <jrm@cin.ufpe.br>
2018-06-10 07:12:56 -0300

    plotando uns graficos diferenciados

--- projeto.py
+++ projeto.py
@@ -1,10 +1,17 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+plt.style.use("ggplot")
 
 acidentes = pd.read_csv(
     "acidentes2017.csv",
     sep=';',header=None, names=["tipo","situacao","data","hora","bairro","endereco","numero","complemento","natureza","descricao","auto","moto","ciclom","ciclista","pedestre","onibus","caminhao","viatura","outros","vitimas","vitimasfatais"])
 
+# acidentes['tipo'].value_counts().plot(kind='barh')
+# plt.show()
+crossAcidentesBairro = pd.crosstab(acidentes['bairro'], acidentes['tipo'])
+crossAcidentesBairro['total'] = crossAcidentesBairro.sum(axis=1)
 
-print acidentes['tipo'].value_counts()
+crossSimples = crossAcidentesBairro
+
+print crossSimples.head()
